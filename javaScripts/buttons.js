5a37c3253376fbb4e65ffe8686c9a9f9f2f04d6e
Intuitive Harmony <j.horst77@gmail.com>
2024-02-07 09:52:48 -0700

    details, about modals

--- javaScripts/buttons.js
+++ javaScripts/buttons.js
@@ -1,19 +1,30 @@
 $(() => {
-  // Toggle pop-up modal and graph
+  // Toggle pop-up modals
   $("#directions-button").click(() => {
-    console.log("Directions Clicked");
-    // $(".directions-container").css({
-    //   display: "block",
-    // });
     $(".overlay").removeClass("hide");
     $(".directions-container").addClass("modal");
     $(".directions-container").removeClass("hide");
+  });
+  $("#details-button").click(() => {
+    $(".overlay").removeClass("hide");
+    $(".details-container").addClass("modal");
+    $(".details-container").removeClass("hide");
+  });
+  $("#about-button").click(() => {
+    $(".overlay").removeClass("hide");
+    $(".about-container").addClass("modal");
+    $(".about-container").removeClass("hide");
   });
   // Close the Modals
   $(".close-modal-toggle").click(() => {
     console.log("modal closed");
     $(".overlay").addClass("hide");
+    // target each container specifically
     $(".directions-container").addClass("hide");
     $(".directions-container").removeClass("modal");
+    $(".details-container").addClass("hide");
+    $(".details-container").removeClass("modal");
+    $(".about-container").addClass("hide");
+    $(".about-container").removeClass("modal");
   });
 });
